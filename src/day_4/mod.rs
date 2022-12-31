9762c00193aec2f1d7ae1d0f7d59f22df31c4d61
rrichy <rasulrichy@gmail.com>
2022-12-31 12:51:19 +0800

    day 4 test

--- src/day_4/mod.rs
+++ src/day_4/mod.rs
@@ -1,9 +1,17 @@
+use std::time::Instant;
+
 use crate::read_input;
 
 pub fn solve() {
     println!("DAY 4");
-    let input = read_input(4);
+    
+    part_one();
+    part_two();
+}
 
+fn part_one() -> u32 {
+    let start = Instant::now();
+    let input = read_input(4);
     let mut total: u32 = 0;
     for range_pair in input.split("\r\n") {
         let vec: Vec<u32> = range_pair
@@ -12,7 +20,6 @@ pub fn solve() {
             .collect();
 
         let (mut min_1, mut max_1, mut min_2, mut max_2) = (vec[0], vec[1], vec[2], vec[3]);
-        // setting the left range to always be the least
         if min_1 > min_2 {
             (min_1, max_1, min_2, max_2) = (min_2, max_2, min_1, max_1);
         }
@@ -21,12 +28,18 @@ pub fn solve() {
             total += 1;
         }
     }
-
     println!(
         "Total assignment pairs that has one range fully contain the other is {}",
         total
     );
+    println!("Solved in: {:?}", start.elapsed());
 
+    total
+}
+
+fn part_two() -> u32 {
+    let start = Instant::now();
+    let input = read_input(4);
     let mut total: u32 = 0;
     for range_pair in input.split("\r\n") {
         let vec: Vec<u32> = range_pair
@@ -35,7 +48,6 @@ pub fn solve() {
             .collect();
 
         let (mut min_1, mut max_1, mut min_2, mut max_2) = (vec[0], vec[1], vec[2], vec[3]);
-        // setting the left range to always be the least
         if min_1 > min_2 {
             (min_1, max_1, min_2, max_2) = (min_2, max_2, min_1, max_1);
         }
@@ -46,6 +58,23 @@ pub fn solve() {
             total += 1;
         }
     }
-
     println!("Total assignment pairs that overlaps is {}", total);
+    println!("Solved in: {:?}", start.elapsed());
+
+    total
+}
+
+#[cfg(test)]
+mod day_four_tests {
+    use super::*;
+
+    #[test]
+    fn part_one_should_be_correct() {
+        assert_eq!(576, part_one(), "Day 4 - Part 1 should be 576");
+    }
+
+    #[test]
+    fn part_two_should_be_correct() {
+        assert_eq!(905, part_two(), "Day 4 - Part 2 should be 905");
+    }
 }
