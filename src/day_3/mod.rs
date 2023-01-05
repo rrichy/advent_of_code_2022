8bf9888ccd832d781c12c92668e8055c07f7ce4a
rrichy <rasulrichy@gmail.com>
2023-01-05 10:58:52 +0800

    [change] day_3 refactor

--- src/day_3/mod.rs
+++ src/day_3/mod.rs
@@ -13,22 +13,22 @@ pub fn solve() {
 pub fn part_one() -> u32 {
     let start = Instant::now();
     let input = read_input(3);
-    let mut total: u32 = 0;
 
-    for rucksacks in input.split("\r\n") {
-        let len = &rucksacks.len() / 2;
-        let set: HashSet<char> = HashSet::from_iter(&mut rucksacks[..len].chars());
-        for c in rucksacks[len..].chars() {
-            if set.contains(&c) {
-                if c.is_ascii_lowercase() {
-                    total += c as u32 - 96;
-                } else {
-                    total += c as u32 - 64 + 26;
+    let total = input.lines()
+        .map(|r| {
+            let (left, right) = r.split_at(r.len() / 2);
+            for c in HashSet::<char>::from_iter(left.chars()) {
+                if right.contains(c) {
+                    let d = c as u32;
+                    return match c.is_ascii_lowercase() {
+                        true => d - 96,
+                        false => d - 64 + 26,
+                    }
                 }
-                break;
             }
-        }
-    }
+            0
+        })
+        .sum();
     println!("Sum: {}, solved in: {:?}", total, start.elapsed());
 
     total
@@ -37,53 +37,23 @@ pub fn part_one() -> u32 {
 fn part_two() -> u32 {
     let start = Instant::now();
     let input = read_input(3);
-    let mut total: u32 = 0;
-    let mut rucksacks = input.split("\r\n");
 
-    loop {
-        let mut should_break = false;
-
-        let mut set = HashSet::new();
-        for _ in 0..3 {
-            match rucksacks.next() {
-                Some(rucksack) => {
-                    if set.capacity() == 0 {
-                        set = HashSet::from_iter(rucksack.chars());
-                    } else {
-                        let set2 = HashSet::from_iter(rucksack.chars());
-                        let set3 = set.clone();
-
-                        set.clear();
-                        for c in set3.intersection(&set2) {
-                            set.insert(*c);
-                        }
+    let total: u32 = input.lines().collect::<Vec<_>>()
+        .chunks(3)
+        .map(|chunk| {
+            for c in HashSet::<char>::from_iter(chunk[0].chars()) {
+                if chunk[1].contains(c) && chunk[2].contains(c) {
+                    let d = c as u32;
+                    return match c.is_ascii_lowercase() {
+                        true => d - 96,
+                        false => d - 64 + 26,
                     }
                 }
-                None => {
-                    should_break = true;
-                    break;
-                }
             }
-        }
-
-        for c in set.into_iter() {
-            if c.is_ascii_lowercase() {
-                total += c as u32 - 96;
-            } else {
-                total += c as u32 - 64 + 26;
-            }
-            break;
-        }
-
-        if should_break == true {
-            break;
-        }
-    }
-    println!(
-        "Sum priorities: {}, solved in: {:?}",
-        total,
-        start.elapsed()
-    );
+            0
+        })
+        .sum();
+    println!("Sum: {}, solved in: {:?}", total, start.elapsed());
 
     total
 }
