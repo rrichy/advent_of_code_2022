2bca2f47e8041cb95ee0786b0bab88f7c03cf568
rrichy <rasulrichy@gmail.com>
2023-01-08 13:59:54 +0800

    day 6

--- src/day_6/mod.rs
+++ src/day_6/mod.rs
@@ -1,4 +1,5 @@
-use std::time::Instant;
+use std::{time::Instant};
+use itertools::Itertools;
 
 use crate::read_txt_file;
 
@@ -12,63 +13,30 @@ pub fn solve() {
 fn part_one() -> usize {
     let start = Instant::now();
     let input = read_txt_file(6, crate::TextEnum::Input);
-    let mut packet = Vec::<char>::from_iter(input.split_at(4).0.chars());
-    let mut first = 0;
-    for (marker, c) in input.char_indices() {
-        let mut is_unique = true;
 
-        packet.splice(..1, []);
-        packet.push(c);
+    let marker = input.as_bytes()
+        .windows(4)
+        .position(|packet| packet.iter().all_unique()).unwrap() + 4;
 
-        for i in 0..4 {
-            let mut sub = packet.clone();
-            sub.remove(i);
-            if sub.contains(&packet.get(i).unwrap()) {
-                is_unique = false;
-                break;
-            }
-        }
-
-        if is_unique {
-            first = marker + 1;
-            break;
-        }
-    }
-    println!("First marker detected after: {}", first);
+    println!("First marker detected after: {}", marker);
     println!("Solved in: {:?}", start.elapsed());
 
-    first
+    marker
 }
 
 fn part_two() -> usize {
     let start = Instant::now();
     let input = read_txt_file(6, crate::TextEnum::Input);
-    let mut message = Vec::<char>::from_iter(input.split_at(14).0.chars());
-    let mut first = 0;
-    for (marker, c) in input.char_indices() {
-        let mut is_unique = true;
 
-        message.splice(..1, []);
-        message.push(c);
-
-        for i in 0..14 {
-            let mut sub = message.clone();
-            sub.remove(i);
-            if sub.contains(&message.get(i).unwrap()) {
-                is_unique = false;
-                break;
-            }
-        }
-
-        if is_unique {
-            first = marker + 1;
-            break;
-        }
-    }
-    println!("First message detected after: {}", first);
+    let message = input.as_bytes()
+        .windows(14)
+        .position(|packet| packet.iter().all_unique())
+        .unwrap() + 14;
+    
+    println!("First message detected after: {}", message);
     println!("Solved in: {:?}", start.elapsed());
 
-    first
+    message
 }
 
 #[cfg(test)]
